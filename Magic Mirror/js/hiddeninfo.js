c620ff00d7d005569ebdc523ada7abb9179731a3
Jackson Bush <jackson@hertzler.com>
2017-12-21 13:41:10 -0500

    pushing hidden info

---
+++ Magic Mirror/js/hiddeninfo.js
@@ -0,0 +1,3 @@
+﻿var clientId = "327785858957-8t0s1cfdo379juri6pgkba07uoaneqcb.apps.googleusercontent.com";
+var clientSecret = "cIB0qGPB1XcUaBY-NcJTHgjJ";
+var refreshToken = "1/76ZLYZinPZS2WTbX5XdzgHA6_w2bpWEgG0Xwpr-N6TE";
