ea7c7464ece188b9a73a586dfbaf95181d9dd62e
Jackson Bush <jackson@hertzler.com>
2018-01-20 17:39:23 -0500

    adding python script and shell scripts for motion detector

---
+++ mirror/pir.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import Rpi.GPIO as io
+import subprocess
+
+io.setmode(io.BCM)
+SHUTOFF_DELAY = 60
+PIR_PIN = 7				# pin 26
+
+def main():
+	io.setup(PIR_PIN, io.IN)
+	turned_off = False
+	last_motion_time = time.time()
+	
+	while True:
+		# signal received
+		if io.input(PIR_PIN):
+			# update last motion time
+			last_motion_time = time.time()
+			
+			# write buffer to terminal
+			sys.stdout.flush()
+			
+			# if it's on and signal received, turn on
+			if turned_off:
+				turned_off = False
+				turn_on()
+			else:
+				# if signal received and it's off and it's been longer than 60 seconds, turn on
+				if not turned_off and time.time() > (last_motion_time + SHUTOFF_DELAY):
+					turned_off = True
+					turn_off()
+			
+			# delay a bit between each reading
+			time.sleep(.1)
+			
+
+def turn_on():
+	subprocess.call('sh /var/www/html/mirror/mirror/turn_on.sh', shell=True)
+
+def turn_off():
+	subprocess.call('sh /var/www/html/mirror/mirror/turn_of.sh', shell=True)
+			
+			
+			
