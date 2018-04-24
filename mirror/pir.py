58ca3aa0777e357920d6f26390e8ec6609f9ec17
Jackson Bush <jackson@hertzler.com>
2018-04-24 18:12:27 -0400

    working with button hopefully

--- mirror/pir.py
+++ mirror/pir.py
@@ -7,45 +7,44 @@ import subprocess
 
 io.setmode(io.BCM)
 SHUTOFF_DELAY = 60
-PIR_PIN = 7				# pin 26
+PIR_PIN = 25
 
 def main():
-	pass
-	#io.setup(PIR_PIN, io.IN)
-	#turned_off = False
-	#last_motion_time = time.time()
+	io.setup(PIR_PIN, io.IN, pull_up_down=io.PUD_UP)
+	turned_off = False
+	last_button_press = time.time()
 	
-	#while True:
+	while True:
 		# signal received
-	#	if io.input(PIR_PIN):
-			# upate time of last motion
-	#		last_motion_time = time.time()
+		if io.input(PIR_PIN):
+			# upate time of last button press
+			last_button_press = time.time()
 			
 			# write buffer to terminal
-	#		sys.stdout.flush()
+			sys.stdout.flush()
 			
 			# if it's off, turn on
-	#		if turned_off:
-	#			turned_off = False
-		#		turn_on()
+			if turned_off:
+				turned_off = False
+				turn_on()
 				
-		#	else:
+			else:
 				# if it's on, turn off
-		#		turned_off = True
-				#turn_off()
+				turned_off = True
+				turn_off()
 				
-			# after turning monitor on or off, wait 10 seconds before reading again (output lasts 5)
-			#time.sleep(10)	
+			# after turning monitor on or off, wait 5 seconds before reading again
+			time.sleep(5)	
 		
 		# signal is off		
-		#else:
+		else:
 			# shut off after 30 mins
-		#	if turned_off == False and time.time() > (last_motion_time + 1800):
-		#		turned_off = True
-			#	turn_off()
+			if turned_off == False and time.time() > (last_button_press + 1800):
+				turned_off = True
+				turn_off()
 		
 		# delay a bit between each reading
-		#time.sleep(.1)
+		time.sleep(.2)
 		
 
 def turn_on():
